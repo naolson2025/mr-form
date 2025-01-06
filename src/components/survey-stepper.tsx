139233895fb4ff071725bc82c652b2226adfa6bb
Nick Olson <naolson.1819@gmail.com>
2025-01-06 09:52:59 -0600

    working on survey stepper

--- src/components/survey-stepper.tsx
+++ src/components/survey-stepper.tsx
@@ -1,8 +1,9 @@
 // import Link from 'next/link';
 import { db } from '@/lib/db';
+import { getSessionCookie } from '../lib/cookies';
 
-// need the number of questions
-// need the url to determine current question
+// need the number of questions ✅
+// need the url to determine current question ✅
 // need to know which questions have been answered (diff color)
 
 type SurveyStepperProps = {
@@ -10,19 +11,25 @@ type SurveyStepperProps = {
   questionId: number;
 };
 
-export default function SurveyStepper(props: SurveyStepperProps) {
-  const numberOfQuestions = db
-    .prepare('SELECT COUNT(*) FROM Questions WHERE survey_id = ?')
-    .get(props.surveyId) as { 'COUNT(*)': number };
+export default async function SurveyStepper(props: SurveyStepperProps) {
+  const surveyQuestions = db
+    .prepare('SELECT id FROM Questions WHERE survey_id = ?')
+    .all(props.surveyId) as { id: number }[];
 
-  console.log(numberOfQuestions);
+  const userId = await getSessionCookie();
+  // get all the questions that have been answered by the user
+  const answeredQuestions = db
+    .prepare(
+      'SELECT question_id FROM Responses WHERE user_id = ?'
+    )
+    .all(userId) as { question_id: number }[];
 
   return (
     <ul className="steps">
-      <li className="step step-primary">Register</li>
-      <li className="step step-primary">Choose plan</li>
-      <li className="step">Purchase</li>
-      <li className="step">Receive Product</li>
+      <li className="step step-primary"></li>
+      <li className="step step-primary"></li>
+      <li className="step"></li>
+      <li className="step"></li>
     </ul>
   );
 }
